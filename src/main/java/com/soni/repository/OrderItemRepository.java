446a2395a1f8e6f8b46ceb248259116e3f777da0
prajwalsoni <prajwalsonics@gmail.com>
2023-12-11 21:43:30 +0530

    OrderItemRepository

---
+++ src/main/java/com/soni/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.soni.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zosh.modal.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
