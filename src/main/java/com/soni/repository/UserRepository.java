4196a099deb0ad788e21837829e9cd494a3ac390
prajwalsoni <prajwalsonics@gmail.com>
2023-12-12 12:09:34 +0530

    adding user repo

---
+++ src/main/java/com/soni/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.soni.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zosh.modal.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	public User findByEmail(String email);
+
+}
