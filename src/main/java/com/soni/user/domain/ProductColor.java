eb9cb3bcd77b74c5680bbdc10cc4cd17e34b74b9
Prajwal Soni <Prajwal Soni@Prajwal>
2023-12-21 00:46:16 +0530

    added ProductColor

---
+++ src/main/java/com/soni/user/domain/ProductColor.java
@@ -0,0 +1,18 @@
+package com.soni.user.domain;
+
+public enum ProductColor {
+
+	 	BLACK,
+	    WHITE,
+	    RED,
+	    GREEN,
+	    BLUE,
+	    YELLOW,
+	    PINK,
+	    PURPLE,
+	    ORANGE,
+	    GREY,
+	    BROWN,
+	    GOLD,
+	    SILVER
+}
