a1eff7b5aae473e87275218f39355339a4ae2009
prajwalsoni <prajwalsonics@gmail.com>
2024-01-18 13:16:20 +0530

    Updated AppConfig

--- src/main/java/com/soni/config/AppConfig.java
+++ src/main/java/com/soni/config/AppConfig.java
@@ -42,7 +42,8 @@ public class AppConfig {
 								
 								"http://localhost:3000", 
 								"http://localhost:4000",
-								"http://localhost:4200"
+								"http://localhost:4200",
+								"https://soniselects.vercel.app/"
 								
 								
 							)
